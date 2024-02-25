dcb83f67897ea0ac5e23601cc480d69f97ed6006
aveNik <158022654+aveNik@users.noreply.github.com>
2024-02-25 19:41:13 +0200

    Fix main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 #hello
 print("maded by tiny_mauss")
 print("-------------------")
@@ -5,40 +7,50 @@ print(" ")
 
 # LCD
 def LCD():
-  a=input("a = ")
-  b=input("b = ")
-  try:
-    #int convert
-    ai=int(a)
-    bi=int(b)  #ai & bi is abreviation of "a interger"... Why i write it? Users of github can understand this shit-code
-    while a*b != 0:
-      if a >= b:
-        a=a+b
-      else:
-        b=b%a
-    LCD=a+b
-    print(LCD)
-  except ValueError:
-    print("NaN")
-    return
+  a=int(input('a= '))
+  b=int(input('b= '))
+  while a*b != 0:
+    if a>=b:
+      a = a % b
+    else:
+      b = b % a
+  LCD=a+b
+  print(LCD)
 
 def Fragment():
-  s=input("")
+  s=input("input text > ")
   n=len(s)
   s1=''
   for i in range(n):
     l=str(s[i])
-    s1+=1
+    s1+=l
     if l==".":
       break
   print(s1)
 
 def NoS():
-  s=input("Input text: ")
+  s=input("Input text > ")
   k=0
-  n=len(s):
+  n=len(s)
   for i in range(n):
     l=str(s[i])
-     if l=='.' or l=="!" or l=="?":
+    if l=='.' or l=="!" or l=="?":
        k=k+1
   print(k)
+
+def main():
+  print("What function you want to try? (1 or 2 or 3):")
+  print("1) LCD; 2) Fragment; 3) NoS")
+  choise = input("> ")
+  if choise == "1":
+    LCD()
+  elif choise == "2":
+    Fragment()
+  elif choise == "3":
+    NoS()
+  else:
+    print("Err: restart after 3 seconds")
+    time.sleep(3)
+    main()
+
+main()
